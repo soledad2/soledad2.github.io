7fbd2aa05169e4ddeccc94c6a5a32386f1c3fb50
Soledad2 <solgutierrezc@gmail.com>
2021-04-27 00:21:16 -0500

    modo nocturno y light

--- script.js
+++ script.js
@@ -1,9 +1,34 @@
 //console.log(document);
 
 const span = document.querySelector("#navbar .menu span");
+const body = document.querySelector("body");
 //console.log(span);
 span.onclick = function () {
     //console.log("click")
-    document.querySelector("body").style.backgroundColor = "#222";
-    document.querySelector("#navbar").style.backgroundColor = "#222";
+
+    if (span.innerHTML == "🐘" ) {
+         //cambia a modo obscuro
+         body.style.backgroundColor = "#222";
+         navbar.style.backgroundColor = "#222";
+         span.innerHTML ="🐝"
+    }
+
+    else if (span.innerHTML == "🐝") {
+        //cambia a modo claro
+        body.style.backgroundColor = "#F7F7FF"
+        navbar.style.backgroundColor = "#F7F7FF"
+        span.innerHTML ="🐘"
+    }
+    
 };
+
+//seleccionar -> transformar/detectar
+
+//backgroundColor
+//camel case
+
+//background-color
+//kebab case
+
+//background_color
+//snake case
